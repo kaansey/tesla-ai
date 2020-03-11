a05985d7a6c9213ea2462052eedfc61322421fa1
Kaan Seyitogullari <kaan.seyitogullari@zalando.ie>
2020-03-11 19:28:08 +0000

    Updates

---
+++ precache-manifest.df3fa37c4814cd603f06707aeae27c03.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1c0cbb1bf366f23aa2758e8355e250",
+    "url": "/tesla/index.html"
+  },
+  {
+    "revision": "d366cdb642feac476fef",
+    "url": "/tesla/static/css/main.052ad02c.chunk.css"
+  },
+  {
+    "revision": "c61e63282a6d7d8acc0e",
+    "url": "/tesla/static/js/2.c8c9095d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tesla/static/js/2.c8c9095d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d366cdb642feac476fef",
+    "url": "/tesla/static/js/main.d87a5212.chunk.js"
+  },
+  {
+    "revision": "c27a39733415ef65dcfd",
+    "url": "/tesla/static/js/runtime-main.ec1d0714.js"
+  },
+  {
+    "revision": "347d0e2a8d751c05ceb454e76803682d",
+    "url": "/tesla/static/media/car_icon.347d0e2a.svg"
+  },
+  {
+    "revision": "d1c4606a9a1ba6a6ce215915a7b40319",
+    "url": "/tesla/static/media/play.d1c4606a.svg"
+  },
+  {
+    "revision": "d6e9bf4da76dc2a884960f07d9145cd5",
+    "url": "/tesla/static/media/reset.d6e9bf4d.svg"
+  }
+]);
